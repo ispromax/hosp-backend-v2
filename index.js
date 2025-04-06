4a121d19aac5f46a0417ff1e7515cfdf095b56eb
Imran <ispromaxrail@gmail.com>
2025-04-06 12:38:40 +0530

    t

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const patientsRoute = require('./routes/patients');
 
 //dotenv.config();
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hospital backend is running!');
 });
 
-app.use('/patients', patientsRoute);
+app.use('/api', require('./routes/patients'));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
--- routes/patients.js
+++ routes/patients.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const pool = require('../db');
 
-router.get('/', async (req, res) => {
+router.post('/register', async (req, res) => {
+  const { first_name, last_name, age, gender, phone, email } = req.body;
+
   try {
-    const result = await db.query('SELECT * FROM patients');
-    res.json(result.rows);
+    const result = await pool.query(
+      `INSERT INTO patients (first_name, last_name, age, gender, phone, email) 
+       VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
+      [first_name, last_name, age, gender, phone, email]
+    );
+
+    res.status(201).json(result.rows[0]);
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    console.error(err);
+    res.status(500).send('Something went wrong!');
   }
 });
 
