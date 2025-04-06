24e6a4a19b44c84e2f577d149de527c61e5bce31
Imran <ispromaxrail@gmail.com>
2025-04-06 13:00:38 +0530

    db changes

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://dname_1_user:7DdOLXTTDSciD8qlRrvRfEMN27iEMfPR@dpg-cvohlmjuibrs73br9stg-a/dname_1' // process.env.DATABASE_URL, // or directly paste the Render DB URL here temporarily
+  connectionString: 'postgresql://dname_1_user:7DdOLXTTDSciD8qlRrvRfEMN27iEMfPR@dpg-cvohlmjuibrs73br9stg-a/dname_1', // process.env.DATABASE_URL, // or directly paste the Render DB URL here temporarily
   ssl: {
     rejectUnauthorized: false
   }
