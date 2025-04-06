ddaa899393c96dfeee568f84da416ea68af88fbf
Imran <ispromaxrail@gmail.com>
2025-04-06 12:58:21 +0530

    db changes

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 const { Pool } = require('pg');
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: 'postgresql://dname_1_user:7DdOLXTTDSciD8qlRrvRfEMN27iEMfPR@dpg-cvohlmjuibrs73br9stg-a/dname_1' // process.env.DATABASE_URL, // or directly paste the Render DB URL here temporarily
   ssl: {
     rejectUnauthorized: false
   }
 });
-
-module.exports = pool;
