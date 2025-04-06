5497dff109ca34b3d5c182eb5df0fab4dbc8374c
Imran <ispromaxrail@gmail.com>
2025-04-06 13:22:08 +0530

    pat data

--- routes/patients.js
+++ routes/patients.js
@@ -10,17 +10,18 @@ router.get('/register', (req, res) => {
 // POST /api/register - actual form submission
 router.post('/register', async (req, res) => {
   const { first_name, last_name, age, gender, phone, email } = req.body;
-
+ console.log("Received registration request:", req.body);
   try {
     const result = await pool.query(
       `INSERT INTO patients (first_name, last_name, age, gender, phone, email) 
        VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
       [first_name, last_name, age, gender, phone, email]
     );
-
+console.log("Insert result:", result.rows[0]);
     res.status(201).json(result.rows[0]);
   } catch (err) {
     console.error(err);
+	console.error("Insert error:", err);
     res.status(500).send('Something went wrong!');
   }
 });
